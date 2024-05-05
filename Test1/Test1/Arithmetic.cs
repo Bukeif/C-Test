ee5b4bd32789583a2e0f7f34b72e57223a1f4d72
Bukeif <az4659811@gmail.com>
2024-05-05 23:57:34 +0800

    add Test1

---
+++ Test1/Test1/Arithmetic.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Arithmetic
+{
+    public class ArithmeticOperations
+    {
+        // 加法
+        public static int Add(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            return Add(sum, carry);
+        }
+
+        // 減法
+        public static int Sub(int a, int b)
+        {
+            b = Add(~b, 1);
+            return Add(a, b);
+        }
+
+        // 乘法
+        public static int Multiply(int a, int b)
+        {
+            int result = 0;
+            for (int i = 0; i < b; i++)
+            {
+                result += a;
+            }
+            return result;
+        }
+        // 除法
+        public static int Divide(int a, int b)
+        {
+            int quotient = 0;
+            int remainder = a;
+
+            while (remainder >= b)
+            {
+                remainder -= b;
+                quotient++;
+            }
+            return quotient;
+        }
+
+        public static void show()
+        {
+            int a = 5;
+            int b = 3;
+
+            int product = Multiply(a, b);
+            Console.WriteLine($"{a}*{b} = {product}");
+        }
+
+    }
+}
---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Arithmetic;
+
+Console.WriteLine("Hello, World!");
+
+int g2 = ArithmeticOperations.Sub(5, 12);
+Console.WriteLine(g2);
+Console.WriteLine("驗算 : " + (5 - 12));
+ArithmeticOperations.show();
+
+
